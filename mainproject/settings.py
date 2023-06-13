bde1fe102b20e29800f7146cbaa658997ffba10a
emanuelthiago06 <emanuel.thiago@academico.ifpb.edu.br>
2023-06-13 19:08:04 -0300

    change

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kh=g^&t03$3d7td007__n#2do087-kj+oafkcki5uk#cxo-u0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','192.168.0.20']
+ALLOWED_HOSTS = ['*','172.18.0.1','0.0.0.0','bla-bla-bla.bla-bla-vla.com', '127.0.0.1', 'localhost','10.0.61.121']
 
 
 # Application definition
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
