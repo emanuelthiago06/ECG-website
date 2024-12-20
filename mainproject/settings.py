cc1f970a48989fa472605242eb683b9779ddbd40
Emanuel <emanuel.thiago@academico.ifpb.edu.br>
2024-12-20 14:50:48 -0300

    update database to postgres

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +80,15 @@ WSGI_APPLICATION = 'mainproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),        # Nome do banco de dados
+        'USER': os.getenv("USERNAME"),              # Usuário do banco
+        'PASSWORD': os.getenv("PASSWORD"),            # Senha do usuário
+        'HOST': os.getenv("HOST"),              # Endereço do servidor (use o IP se estiver em outro lugar)
+        'PORT': os.getenv("PORT"),                   # Porta padrão do PostgreSQL
     }
 }
 
